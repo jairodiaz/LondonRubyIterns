765fb1fc3ea760793c195e6df4a2cf383bca9258
Jairo Diaz <jairodiaz@Jairos-MacBook-Air.local>
2012-02-19 17:36:01 +0000

    Configured default page for index

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Rubyintern2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'internships#index'
 
   # See how all your routes lay out with "rake routes"
 
